46bc1e14caaeb8149a61f81a76ecfef30d0bc2f3
Maayan Shalom <sha.maayan@gmail.com>
2023-02-23 16:18:47 +0200

    freehandElement.points

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import './App.css';
 import {
   Excalidraw,
 } from "@excalidraw/excalidraw";
-import { getFreeDrawSvgPath } from "@excalidraw/excalidraw";
+// import { getFreeDrawSvgPath } from "@excalidraw/excalidraw";
 import { ExcalidrawElement, ExcalidrawFreeDrawElement } from "@excalidraw/excalidraw/types/element/types";
 
 function App() {
-  const handleSceneUpdate = (elements: readonly ExcalidrawElement[], appState: any) => {
+  const handleSceneUpdate = (elements: readonly ExcalidrawElement[]) => {
     elements.forEach(element => {
       if (element.type === "freedraw") {
         const freehandElement = element as ExcalidrawFreeDrawElement;
-        const path = getFreeDrawSvgPath(freehandElement);
-        console.log(path);
+        // const path = getFreeDrawSvgPath(freehandElement);
+        console.log(freehandElement.points);
       }
     });
   };
